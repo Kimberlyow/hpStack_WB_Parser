0cd5a07bcf04f44b7a251ad2a57bc3aeacf6cdb0
Kimberlyow <kimberlymodl04@gmail.com>
2022-11-28 14:28:26 +0100

    Update #W.15.14.0014.js

--- #W.15.14.0014.js
+++ #W.15.14.0014.js
@@ -6,26 +6,64 @@ const hostname = data.match(/(?<=hostname\s").+(?=")/m)[0];
 
 let hpTrunk = data.match(/(?<=trunk\s).+(?=\slacp)/gm);
 
-let ports = [];
+let trkInfo = [];
+
+function unhyphen(line) {
+  let firstInt = parseInt(line.match(/\d+(?=\-)/)[0]);
+  let lastInt = parseInt(line.match(/(?<=-)\d+/)[0]);
+  let sequence = [];
+  for (let i = 0; i <= lastInt-firstInt; i++) {
+    sequence = [...sequence, firstInt+i];
+  }
+  return sequence;
+}
 
 hpTrunk.forEach(trkLine => {
   let trunk = trkLine.match(/trk\d+/)[0];
   if (trkLine.includes("-")) {
     if (trkLine.includes("A"||"B") && !trkLine.includes("/")) {
-      let port = {trunkID:trunk, portID:trkLine.match(/(?<=^|\-)[AB]\d/g)};
-      ports = [...ports, port];
-    } else if (!trkLine.includes("A"||"B") && !trkLine.includes("/")) {
-      let firstInt = parseInt(trkLine.match(/\d+(?=\-)/)[0]);
-      let lastInt = parseInt(trkLine.match(/(?<=-)\d+/)[0]);
-      let sequence = [];
-      for (let i = 0; i <= lastInt-firstInt; i++) {
-        sequence = [...sequence, firstInt+i];
-      }
-      let port = {trunkID:trunk, portID:sequence}
-      ports = [...ports, port];
+
+      trkInfo = [...trkInfo, {trunkID:trunk, portID:trkLine.match(/(?<=^|\-)[AB]\d/g)}];
+
+    } else if (!trkLine.includes("A"||"B"||"/")) {
+
+      let sequence = unhyphen(trkLine);
+      trkInfo = [...trkInfo, {trunkID:trunk, portID:sequence}];
+
     }
   }
 })
 
-console.log(hostname, ports)
+let vlans = [];
+let ports = [];
+
+let splitExc = data.split("\nvlan");
+splitExc.shift();
+splitExc.forEach(interface => {
+  //link ID with name
+  let vlanID = parseInt(interface.match(/\d+/)[0]);
+  let vlanName = interface.match(/(?<=name\s").+(?=")/)[0];
+  vlans = [...vlans, {ID:vlanID, name:vlanName}];
+
+  interface.split("\n").forEach(line => {
+    if (line.includes("untagged")) {
+      if (line.includes("-")) {
+        if (!line.includes("A"||"B"||"/")) {
+
+          //to-do: push this into an object?
+          let sequence = unhyphen(line);
+          sequence.forEach(portID => {
+            console.log(`port:${portID} vlan:${vlanID}`);
+          })
+
+        }
+      }
+    } else if (line.includes("tagged")) {
+
+    }
+  })
+
+});
+
+console.log(hostname, trkInfo, vlans)
 return null;
