5ea3bc643f78780aeb32276d7b3a30df63d6e62e
Kimberlyow <kimberlymodl04@gmail.com>
2022-11-28 13:18:55 +0100

    Added Trunk Groups

--- #W.15.14.0014.js
+++ #W.15.14.0014.js
@@ -4,5 +4,28 @@ const data = fs.readFileSync('./example_config.txt', {encoding:'utf8', flag:'r'}
 
 const hostname = data.match(/(?<=hostname\s").+(?=")/m)[0];
 
-console.log(hostname)
+let hpTrunk = data.match(/(?<=trunk\s).+(?=\slacp)/gm);
+
+let ports = [];
+
+hpTrunk.forEach(trkLine => {
+  let trunk = trkLine.match(/trk\d+/)[0];
+  if (trkLine.includes("-")) {
+    if (trkLine.includes("A"||"B") && !trkLine.includes("/")) {
+      let port = {trunkID:trunk, portID:trkLine.match(/(?<=^|\-)[AB]\d/g)};
+      ports = [...ports, port];
+    } else if (!trkLine.includes("A"||"B") && !trkLine.includes("/")) {
+      let firstInt = parseInt(trkLine.match(/\d+(?=\-)/)[0]);
+      let lastInt = parseInt(trkLine.match(/(?<=-)\d+/)[0]);
+      let sequence = [];
+      for (let i = 0; i <= lastInt-firstInt; i++) {
+        sequence = [...sequence, firstInt+i];
+      }
+      let port = {trunkID:trunk, portID:sequence}
+      ports = [...ports, port];
+    }
+  }
+})
+
+console.log(hostname, ports)
 return null;
